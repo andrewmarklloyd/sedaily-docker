fa3736634a91e8109785eb05d284edfbd0801b80
Andrew Lloyd <andrew.lloyd@tee2.org>
2018-04-27 12:56:34 -0700

    Add docker services to .travis

--- backup-mongo.sh
+++ backup-mongo.sh
@@ -2,6 +2,6 @@
 
 # use mongo docker container to run mongodump
 export COMMAND="mongodump --host $MONGO_HOST -d $MONGO_DB --port $MONGO_PORT --username $MONGO_USERNAME --password $MONGO_PASSWORD --out /home/dump --excludeCollection users"
-sudo docker run -v `pwd`/dump:/home/dump --rm -it mongo:3.4.10 bash -c "$COMMAND"
+docker run -v `pwd`/dump:/home/dump --rm -it mongo:3.4.10 bash -c "$COMMAND"
 # tar and compress the dump
 tar -czf dump.tar.gz ./dump/
