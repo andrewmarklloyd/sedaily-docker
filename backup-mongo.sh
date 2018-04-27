15dae79d45463109c4ec3a5a70d223051f3755e9
Andrew Lloyd <andrew.lloyd@tee2.org>
2018-04-27 13:37:39 -0700

    Use the DB name to build the tar file and directory

--- backup-mongo.sh
+++ backup-mongo.sh
@@ -2,8 +2,8 @@
 
 # use mongo docker container to run mongodump
 export COMMAND="mongodump --host $MONGO_HOST -d $MONGO_DB --port $MONGO_PORT --username $MONGO_USERNAME --password $MONGO_PASSWORD --out /home/dump --excludeCollection users"
-docker run -v `pwd`/dump:/home/dump --rm -it mongo:3.4.10 bash -c "$COMMAND"
+docker run -v `pwd`/:/home/dump --rm -it mongo:3.4.10 bash -c "$COMMAND"
 # tar and compress the dump
 mkdir -p target/latest
-tar -czf dump.tar.gz ./dump/
-mv dump.tar.gz target/latest/
+tar -czf $MONGO_DB.tar.gz ./$MONGO_DB/
+mv $MONGO_DB.tar.gz target/latest/
